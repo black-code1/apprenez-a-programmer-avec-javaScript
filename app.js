7cb7f34329f5530a1c36e9fc15b9f1c36f30fc5d
black-code1 <legrandmahamat1@gmail.com>
2023-01-07 08:34:36 +0100

    Pratiquez l'utilisation des opérateurs

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-// Create your variables here
-// ==========================================
-
 let numberOfSeasons = 6;
-let numberOfEpisodes = 12
+let numberOfEpisodes = 12;
 
-// ==========================================
+// Calculate totalShowTime here
+// =====================================
+
+let episodeTime = 45;
+let commercialTime = 5;
+
+let totalShowTime = (episodeTime + commercialTime) * numberOfSeasons * numberOfEpisodes;
+
+// =====================================
 
 let paragraph = document.querySelector('#info');
-paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`
+paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season
+
+Total viewing time: ${totalShowTime} minutes`
