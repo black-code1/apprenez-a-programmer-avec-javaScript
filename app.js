13f3c2ccc40c006fdd5b40c2a40049ccf09c2960
black-code1 <legrandmahamat1@gmail.com>
2023-01-07 10:01:56 +0100

    Pratiquez les classes

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-let episode = {
-    title: 'Dark Beginnings',
-    duration: 45,
-    hasBeenWatched: false
-};
-
-// Create variables here
+// Create a class here
 // =====================================
-let episodeTitle = episode.title;
-let episodeDuration = episode.duration;
-let episodeHasBeenWatched = episode.hasBeenWatched;
+class Episode{
+    constructor(title, duration, hasBeenWatched) {
+        this.title = title;
+        this.duration = duration;
+        this.hasBeenWatched = hasBeenWatched
+    }
+}
+
+let firstEpisode = new Episode('episode 1', 15, true);
+let secondEpisode = new Episode('episode 2', 30, true);
+let thirdEpisode = new Episode('episode 3', 45, false);
+
 
 // =====================================
 
-document.querySelector('#episode-info').innerText = `Episode: ${episodeTitle}
-Duration: ${episodeDuration} min
-${episodeHasBeenWatched ? 'Already watched' : 'Not yet watched'}`
+document.querySelector('#first-episode-info').innerText = `Episode: ${firstEpisode.title}
+Duration: ${firstEpisode.duration} min
+${firstEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+
+document.querySelector('#second-episode-info').innerText = `Episode: ${secondEpisode.title}
+Duration: ${secondEpisode.duration} min
+${secondEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+
+document.querySelector('#third-episode-info').innerText = `Episode: ${thirdEpisode.title}
+Duration: ${thirdEpisode.duration} min
+${thirdEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
