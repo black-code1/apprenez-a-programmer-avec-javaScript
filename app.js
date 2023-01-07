a289d19cea48a3a76688796c32538e5b00c5d14c
black-code1 <legrandmahamat1@gmail.com>
2023-01-07 10:31:26 +0100

    Pratiquez la création d'un array

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-// Create a class here
-// =====================================
-class Episode{
+class Episode {
     constructor(title, duration, hasBeenWatched) {
         this.title = title;
         this.duration = duration;
-        this.hasBeenWatched = hasBeenWatched
+        this.hasBeenWatched = hasBeenWatched;
     }
 }
 
-let firstEpisode = new Episode('episode 1', 15, true);
-let secondEpisode = new Episode('episode 2', 30, true);
-let thirdEpisode = new Episode('episode 3', 45, false);
+let firstEpisode = new Episode('Dark Beginnings', 45, true);
+let secondEpisode = new Episode('The Mystery Continues', 45, false);
+let thirdEpisode = new Episode('An Unexpected Climax', 60, false);
+
+// Create your array here
+// ====================================
+let episodes = [firstEpisode, secondEpisode, thirdEpisode]
 
 
-// =====================================
+// ====================================
 
-document.querySelector('#first-episode-info').innerText = `Episode: ${firstEpisode.title}
-Duration: ${firstEpisode.duration} min
-${firstEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+const body = document.querySelector('body');
 
-document.querySelector('#second-episode-info').innerText = `Episode: ${secondEpisode.title}
-Duration: ${secondEpisode.duration} min
-${secondEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
-
-document.querySelector('#third-episode-info').innerText = `Episode: ${thirdEpisode.title}
-Duration: ${thirdEpisode.duration} min
-${thirdEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+for(let i = 0; i < 12; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('series-frame');
+    let newTitle = document.createElement('h2');
+    newTitle.innerText = 'The Story of Tau';
+    let newParagraph = document.createElement('p');
+    newParagraph.innerText = `${episodes[i].title}
+${episodes[i].duration} minutes
+${episodes[i].hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
+    newDiv.append(newTitle);
+    newDiv.append(newParagraph);
+    body.append(newDiv);
+}
