27beef42dd2a02f5f7095ad548046b1ed17a71dc
black-code1 <legrandmahamat1@gmail.com>
2023-01-07 10:42:20 +0100

    Pratiquez le travail avec les arrays

--- app.js
+++ app.js
@@ -10,24 +10,28 @@ let firstEpisode = new Episode('Dark Beginnings', 45, true);
 let secondEpisode = new Episode('The Mystery Continues', 45, false);
 let thirdEpisode = new Episode('An Unexpected Climax', 60, false);
 
-// Create your array here
+// Modify the array here
 // ====================================
-let episodes = [firstEpisode, secondEpisode, thirdEpisode]
-
+let episodes = [];
+episodes.push(firstEpisode, secondEpisode, thirdEpisode, thirdEpisode);
+episodes.pop()
+let numberOfEpisodes = episodes.length
 
 // ====================================
 
 const body = document.querySelector('body');
 
-for(let i = 0; i < 12; i++) {
+document.querySelector('#episodes').innerText = numberOfEpisodes;
+
+for(let episode of episodes) {
     let newDiv = document.createElement('div');
     newDiv.classList.add('series-frame');
     let newTitle = document.createElement('h2');
     newTitle.innerText = 'The Story of Tau';
     let newParagraph = document.createElement('p');
-    newParagraph.innerText = `${episodes[i].title}
-${episodes[i].duration} minutes
-${episodes[i].hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
+    newParagraph.innerText = `${episode.title}
+${episode.duration} minutes
+${episode.hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
     newDiv.append(newTitle);
     newDiv.append(newParagraph);
     body.append(newDiv);
