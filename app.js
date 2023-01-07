3cd7008169786228e9a4e117f15c0b5f72f70e06
black-code1 <legrandmahamat1@gmail.com>
2023-01-07 14:14:45 +0100

    Pratiquez les bouclesfor

--- app.js
+++ app.js
@@ -1,52 +1,39 @@
-//On pointe sur l'élément de message
-const espaceMessage = document.getElementById("message");
-//On pointe sur l'élément de bouton
-const bouton = document.getElementById("bouton");
-//On pointe sur l'élément de champ de saisie de l'age
-const ageInput = document.getElementById("age");
-//On défini la variage age qu'on utilisera et un variable définissant l'age de la majorité
-let age;
-let ageMajorite = 18;
-
-
-//Cette fonction affichera le message de validation
-function valider(){
-    espaceMessage.innerHTML = "Vous êtes autorisé à entrer";
-}
-
-//Cette fonction affichera un message d'erreur
-function refuser(){
-    alert("Cette espace est interdit aux personnes mineurs");
-}
-
-
-
-function onConfirm(){
-
-    //On récupère la saisie de l'age et on transforme le texte en nombre entier
-    age = parseInt(ageInput.value);
-    //Si la saisie n'est pas un nombre, on affiche un message d'erreur
-    if(isNaN(age)){
-        alert("Ceci n'est pas un nombre");
-        return;
+class Episode {
+    constructor(title, duration, hasBeenWatched) {
+        this.title = title;
+        this.duration = duration;
+        this.hasBeenWatched = hasBeenWatched;
     }
+}
 
-    //====Code à rédiger ici======
+let episodes = [
+    new Episode('Dark Beginnings', 45, true),
+    new Episode('The Mystery Continues', 45, false),
+    new Episode('An Unexpected Climax', 60, false)
+];
 
-    //si non majeur apeller la fonction refuser(), sinon apeller la fonction valider()
-    if (age < ageMajorite){
-        refuser()
-    } else {
-        valider()
-    }
-    //=======/Code à rédiger ici======
+// Add logic here
+// ======================
+for (let episode of episodes){
 
-
-    //On vide le champ de saisie
-    ageInput.value = "";
+    episode.hasBeenWatched = false
 }
 
 
+// ======================
 
-//On écoute l'action de click sur le bouton et on appelle la fonction onConfirm
-bouton.addEventListener('click', onConfirm);
+const body = document.querySelector('body');
+
+for(let episode of episodes) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('series-frame');
+    let newTitle = document.createElement('h2');
+    newTitle.innerText = 'The Story of Tau';
+    let newParagraph = document.createElement('p');
+    newParagraph.innerText = `${episode.title}
+${episode.duration} minutes
+${episode.hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
+    newDiv.append(newTitle);
+    newDiv.append(newParagraph);
+    body.append(newDiv);
+}
