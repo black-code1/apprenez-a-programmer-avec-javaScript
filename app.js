5c6fbc94baa9c2a98253f5454bf398f1ade8672d
black-code1 <legrandmahamat1@gmail.com>
2023-01-18 00:44:49 +0100

    Définissez des méthodes d'instance et des propriétés

--- app.js
+++ app.js
@@ -1,28 +1,85 @@
 // Modify the code here
 // ======================
 
-const calculateAverageRating = (ratings) => {
-    if(ratings.length === 0) {
-        return 0;
+class Show {
+    constructor(title, numberOfSeasons) {
+        this.title = title;
+        this.numberOfSeasons = numberOfSeasons;
+        this.ratings = [];
+        this.averageRating = 0;
     }
-    let sum = 0;
-    for (let rating of ratings) {
-        sum += rating;
+
+    addRating(rating) {
+        this.ratings.push(rating)
+        let sum = 0;
+        for (let rating of this.ratings) {
+            sum += rating
+        }
+
+        this.averageRating = sum / this.ratings.length
     }
-     return sum / ratings.length;
+
 
 }
 
 // ======================
 
-const tauRatings = [5, 4, 5, 5, 1, 2];
-const colinRatings = [5, 5, 5, 4, 5];
+const tau = new Show('The Story of Tau', 5);
+const colin = new Show('The Hero of Old Meldrum', 3);
+const clara = new Show('The Bugs of Isla Clara', 6);
 
-const tauAverage = calculateAverageRating(tauRatings);
-const colinAverage = calculateAverageRating(colinRatings);
+const shows = [tau, colin, clara];
 
-if (tauAverage && colinAverage) {
-    document.querySelector('#tau-score').innerText = tauAverage.toFixed(1) + ' stars';
-    document.querySelector('#colin-score').innerText = colinAverage.toFixed(1) + ' stars';
-    document.querySelector('#clara-score').innerText = `${calculateAverageRating([]) === 0 ? 'No ratings' : calculateAverageRating([]) + ' stars'}`
+const body = document.querySelector('body');
+const refresh = document.querySelector('#refresh');
+
+refresh.addEventListener('click', () => {
+    removeShows();
+    addRandomRatings();
+    updateShows();
+})
+
+const updateShows = () => {
+    for (let show of shows) {
+        const showPane = document.createElement('div');
+        showPane.classList.add('series-frame');
+        const showHeading = document.createElement('h2');
+        showHeading.innerText = show.title;
+        const showDetails = document.createElement('p');
+        const seasons = document.createElement('p');
+        seasons.innerText = show.numberOfSeasons + ' seasons';
+        const ratings = document.createElement('p');
+        ratings.innerText = show.averageRating > 0 ? show.ratings.length + ' ratings\n' + show.averageRating.toFixed(1) + ' stars' : 'No ratings yet';
+        showDetails.append(seasons);
+        showDetails.append(ratings);
+        showPane.append(showHeading);
+        showPane.append(showDetails);
+        body.append(showPane);
+    }
+};
+
+const removeShows = () => {
+    const children = [];
+    for (let childNode of body.childNodes) {
+        children.push(childNode);
+    }
+    for (let child of children) {
+        if (child.tagName == 'DIV') {
+            body.removeChild(child);
+        }
+    }
 }
+
+const addRandomRatings = () => {
+    for (let show of shows) {
+        if (Math.random() >= 0.2) {
+            const numberOfRatings = Math.floor(Math.random() * 4 + 1);
+            for (let i = 0; i < numberOfRatings; i++) {
+                const rating = Math.floor(Math.random() * 5 + 1);
+                show.addRating(rating);
+            }
+        }
+    }
+}
+
+updateShows();
