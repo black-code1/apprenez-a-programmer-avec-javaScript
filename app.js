db266834853a872f48e43d36bf7a6538869968fd
black-code1 <legrandmahamat1@gmail.com>
2023-01-18 14:21:41 +0100

    Découvrez la récursivité : l'appel de fonctions à l'intérieur d'elles-mêmes

--- app.js
+++ app.js
@@ -1,60 +1,33 @@
-class Show {
-    constructor(title, numberOfSeasons, episodesPerSeason) {
-        this.title = title;
-        this.numberOfSeasons = numberOfSeasons;
-        this.episodesPerSeason = episodesPerSeason;
+//On pointe sur l'élément de message
+const espaceMessage = document.getElementById("message");
+//On pointe sur l'élément de bouton
+const bouton = document.getElementById("bouton");
+//On pointe sur l'élément de champ de saisie de l'année
+const numberInput = document.getElementById("number");
+
+
+//On défini la variage année qu'on utilisera et un variable définissant l'age de la majorité
+let number;
+
+//======FONCTION À ECRIRE======
+function factorielle(number){
+    if(number <= 1) return 1;
+    else return (number * factorielle(number-1));
+}
+//======/FONCTION À ECRIRE======
+
+function onCalcul(){
+    //On récupère la saisie de l'année et on transforme le texte en nombre entier
+    number = parseInt(numberInput.value);
+    //Si la saisie n'est pas un nombre, on affiche un message d'erreur
+    if(isNaN(number)){
+        alert("Ceci n'est pas un nombre");
+        return;
     }
+    espaceMessage.innerHTML = "La factorielle de " + number + " est " + factorielle(number);
 }
 
-const tau = new Show('The Story of Tau', 5, 12);
-const meldrum = new Show('The Hero of Old Meldrum', 3, 6);
-const clara = new Show('The Bugs of Isla Clara', 6, 15);
 
-const shows = [tau, meldrum, clara];
 
-// Modify the following code
-// ======================
-
-const generateComponent = (show) => {
-    const titleText = show.title;
-    const seasonsText = show.numberOfSeasons + ' Seasons';
-    const episodesText = show.episodesPerSeason + ' episodes per season'
-
-    return {
-        titleText,
-        seasonsText,
-        episodesText
-    }
-}
-
-const tauComponent = generateComponent(tau)
-const meldrumComponent = generateComponent(meldrum)
-const claraComponent = generateComponent(clara)
-
-const showComponents = [tauComponent, meldrumComponent, claraComponent];
-
-// ======================
-// Modify the code above
-
-const body = document.querySelector('body');
-
-const updateShows = () => {
-    for (let show of showComponents) {
-        const showPane = document.createElement('div');
-        showPane.classList.add('series-frame');
-        const showHeading = document.createElement('h2');
-        showHeading.innerText = show.titleText;
-        const showDetails = document.createElement('p');
-        const seasons = document.createElement('p');
-        seasons.innerText = show.seasonsText;
-        const episodes = document.createElement('p');
-        episodes.innerText = show.episodesText;
-        showDetails.append(seasons);
-        showDetails.append(episodes);
-        showPane.append(showHeading);
-        showPane.append(showDetails);
-        body.append(showPane);
-    }
-};
-
-updateShows();
+//On écoute l'action de click sur le onConvert et on appelle la fonction onConvert
+bouton.addEventListener('click', onCalcul);
